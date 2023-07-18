87da194ca3dde3af3c637232de6dd476348090cb
farhadctg22201~ <farhadctg222@gmail.com>
2023-07-18 10:27:30 -0700

    d

---
+++ null-vs-undefine.js
@@ -0,0 +1,26 @@
+// let pakhi;
+
+// function my(num,num1){
+//     console.log(num+num1)
+// }
+// const result = my(11,22)
+
+// function my(num, num2){
+//    const all =  num + num2
+//    return
+    
+    
+// }
+// function my(num,num1=10){
+//     console.log(num,num1)
+// }
+
+// const name = {
+//     name: 'farhad',
+//     id: 45445
+// }
+// let array = [11,22,33,44]
+
+// null vs undefined defination class praktice
+
+console.log(array[22])
