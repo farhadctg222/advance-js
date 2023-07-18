4935e81b5c06b2256a5c9711a21e193b0a8f27e2
farhadctg22201~ <farhadctg222@gmail.com>
2023-07-18 10:00:41 -0700

    first commit

---
+++ truthy.js
@@ -0,0 +1,20 @@
+//false value
+// false
+// 0
+// undefined
+// null
+// NaN
+// ture value
+// ' '
+// '0'
+// 'false'
+// {}
+// []
+// let num = []
+// let a = {}
+const num = 4
+if(num){
+    console.log('conditon is true')
+}else{
+    console.log('conditon is not true')
+}
